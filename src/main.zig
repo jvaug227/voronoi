a310501ffca7f36458e840a1c841d7ed4fe89297
jvaug227 <jtvaughan227@gmail.com>
2025-08-26 13:19:52 -0400

    Looks like a voronoi diagram! Better float handling, euclidian distance, and color channels mean something.
    The program handles floats better for arbitrary canvas size.
    Distance is calculated using euclidian distance rather than manhattan
    distance.
    No more if statement controlling which colors are set, color channels
    are now used to indicate different concepts:
    - Red is distance to nearest point.
    - Green is (nearest_point_idx / point_count)
    - Blue is (min_distance < point_threshold)

--- src/main.zig
+++ src/main.zig
@@ -34,9 +34,6 @@ pub fn main() !void {
         _ = gpa.deinit();
     }
 
-    const pixel_buffer = try alloc.alloc(Pixel, GRID_WIDTH * GRID_HEIGHT);
-    defer alloc.free(pixel_buffer);
-
     const points = try alloc.alloc(Point, POINT_COUNT);
     defer alloc.free(points);
 
@@ -47,25 +44,36 @@ pub fn main() !void {
     for (points) |*point| {
         point.x = rand.float(f32);
         point.y = rand.float(f32);
-        std.log.info("Point: {d} {d}", .{point.x, point.y});
+        std.log.info("Point: {d} {d}", .{ point.x, point.y });
     }
+
+    const pixel_buffer = try alloc.alloc(Pixel, GRID_WIDTH * GRID_HEIGHT);
+    defer alloc.free(pixel_buffer);
 
     for (0..GRID_HEIGHT) |y| {
         for (0..GRID_WIDTH) |x| {
-            var min_distance: usize = 1000;
-            const fx: f32 = @floatFromInt(x);
-            const fy: f32 = @floatFromInt(y);
-            for (points) |point| {
-                const px: f32 = @floor(point.x * @as(f32, @floatFromInt(GRID_WIDTH)));
-                const py: f32 = @floor(point.y * @as(f32, @floatFromInt(GRID_HEIGHT)));
-                const distance_to_point: usize = @intFromFloat(@abs(fx - px) + @abs(fy - py));
-                min_distance = @min(min_distance, distance_to_point);
+            var min_distance: f32 = 1.0;
+            var min_point: usize = 0;
+            const fx: f32 = @as(f32, @floatFromInt(x)) / @as(f32, @floatFromInt(GRID_WIDTH));
+            const fy: f32 = @as(f32, @floatFromInt(y)) / @as(f32, @floatFromInt(GRID_HEIGHT));
+            for (0.., points) |idx, point| {
+                const px: f32 = point.x;
+                const py: f32 = point.y;
+                const distance_to_point = @sqrt(std.math.pow(f32, @abs(fx - px), 2.0) + std.math.pow(f32, @abs(fy - py), 2.0));
+                if (distance_to_point < min_distance) {
+                    min_distance = distance_to_point;
+                    min_point = idx;
+                }
             }
-            if (min_distance <= 2) {
-                pixel_buffer[x + y * GRID_WIDTH] = Pixel{ .r = 0, .g = 0, .b = 255 };
-            } else {
-                pixel_buffer[x + y * GRID_WIDTH] = Pixel{ .r = 50, .g = 20, .b = 20 };
-            }
+            // if (min_distance <= 0.005) {
+            //     pixel_buffer[x + y * GRID_WIDTH] = Pixel{ .r = 0, .g = 0, .b = 255 };
+            // } else {
+            const red: f32 = 255.0 * (1.0 - min_distance);
+            const green: f32 = 255.0 * @as(f32, @floatFromInt(min_point)) / @as(f32, @floatFromInt(POINT_COUNT));
+            const blue_modifier: f32 = if (min_distance < 0.005) 1.0 else 0.0;
+            const blue: f32 = 255.0 * blue_modifier;
+            pixel_buffer[x + y * GRID_WIDTH] = Pixel{ .r = @intFromFloat(red), .g = @intFromFloat(green), .b = @intFromFloat(blue) };
+            // }
         }
     }
 
